bff3ca93d98679084fc072efbfe350251afc4a99
Sulic Octavian <octavius1992@gmail.com>
2022-08-05 09:40:50 +0300

    Add replay button

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function getComputerChoice() {
     let choices = ["Rock", "Paper", "Scissors"];
     let random = Math.floor(Math.random() * choices.length);
-    let computerChoice = (random, choices[random]);
-    return(computerChoice);
+    return choices[random];
 }
 
 var round = 0;
@@ -18,39 +17,51 @@ function playRound(a, b) {
     if (a == "Rock" && b == "Paper") {
         message = loseMsg;
         roboScore += 1;
-        scoreBoard.textContent = `${message}
-        The score is: You-${humanScore} Computer-${roboScore}`
+        mess.textContent = message;
+        scoreBoard.textContent = `The score is: You-${humanScore} Computer-${roboScore}`;
     } else if (a == "Paper" && b == "Scissors") {
         message = loseMsg;
         roboScore += 1;
-        scoreBoard.textContent = `${message} 
-        The score is: You-${humanScore} Computer-${roboScore}`
+        mess.textContent = message;
+        scoreBoard.textContent = `The score is: You-${humanScore} Computer-${roboScore}`;
     } else if (a == "Scissors" && b == "Rock") {
         message = loseMsg;
         roboScore += 1;
-        scoreBoard.textContent = `${message} 
-        The score is: You-${humanScore} Computer-${roboScore}`
+        mess.textContent = message;
+        scoreBoard.textContent = `The score is: You-${humanScore} Computer-${roboScore}`;
     } else if (a == b) {
         message = tieMsg;
-        scoreBoard.textContent = `${message} 
-         The score is: You-${humanScore} Computer-${roboScore}`
+        mess.textContent = message;
+        scoreBoard.textContent = `The score is: You-${humanScore} Computer-${roboScore}`;
     } else {
         message = winMsg;
         humanScore += 1;;
-        scoreBoard.textContent = `${message} 
-        The score is: You-${humanScore} Computer-${roboScore}`
+        mess.textContent = message;
+        scoreBoard.textContent = `The score is: You-${humanScore} Computer-${roboScore}`;
     }
     if (humanScore == 5 ) {
         humanScore = 0;
         roboScore = 0;
-        scoreBoard.textContent = "Congratulations! You won!!!"
+        body.removeChild(content);
+        body.appendChild(endWindow);
+        winLose.textContent = 'Congratulations! You won the game!!!'
+        replay.addEventListener('click', function(){ playAgain()});
     }
     if (roboScore == 5 ) {
         humanScore = 0;
         roboScore = 0;
-        scoreBoard.textContent = "Oh my! Computers are taking over the world!!!"
+        body.removeChild(content);
+        body.appendChild(endWindow);
+        winLose.textContent = 'Oh my! Computers are taking over the world!'
+        replay.addEventListener('click', function(){ playAgain()});
     }
 }
+
+function playAgain() {
+    body.removeChild(endWindow);
+    body.appendChild(content);
+}
+
 
 const rock = document.querySelector('#rock');
 rock.addEventListener('click', function(){ playRound("Rock", getComputerChoice())});
@@ -62,5 +73,16 @@ const scissors = document.querySelector('#scissors');
 scissors.addEventListener('click', function(){ playRound("Scissors", getComputerChoice())});
 
 const content = document.querySelector('#content');
+const mess = document.createElement('p');
 const scoreBoard = document.createElement('h3');
+content.appendChild(mess);
 content.appendChild(scoreBoard);
+
+const body = document.querySelector('body');
+const endWindow = document.createElement('div');
+const winLose = document.createElement('h1');
+const replay = document.createElement('button');
+replay.textContent = 'Play Again? ^_^';
+endWindow.appendChild(winLose);
+endWindow.appendChild(replay);
+
